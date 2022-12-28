15cf079d08ef27162e49aa4f61b9bd2761b5f1c9
BidanGitau <bydangitau@gmail.com>
2022-12-28 14:40:31 +0300

    inter test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
  class CategoryTest <  ActiveSupport::TestCase
     def setup
-        @category =Category.new(name:"Sport")
+        @category =Category.new(name:"Sports")
     end
    test "category should be valid" do
     
@@ -16,7 +16,12 @@ require 'test_helper'
     @category2=Category.new(name: "Sports")
     assert_not @category2.valid?
    end
-    test "name should not be short" do
-        
+    test "name should not be  too long" do
+        @category.name="a"*26
+        assert_not @category.valid?
+    end
+    test "name should not be  too short" do
+        @category.name="aa"
+        assert_not @category.valid? 
     end
  end
